46d9b0eed40557b4fd381f93571fc85c6e0d5077
Pascal Dürsteler <pascal@nimius.net>
2016-06-15 11:51:02 +0200

    CLEANUP | Fix indentation in ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -21,9 +21,9 @@ CREATE TABLE tx_workshops_domain_model_workshop (
     sorting int(11) default '0' NOT NULL,
     crdate int(11) default '0' NOT NULL,
     cruser_id int(11) default '0' NOT NULL,
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l10n_parent int(11) DEFAULT '0' NOT NULL,
-	l10n_diffsource mediumtext,
+    sys_language_uid int(11) DEFAULT '0' NOT NULL,
+    l10n_parent int(11) DEFAULT '0' NOT NULL,
+    l10n_diffsource mediumtext,
 
     PRIMARY KEY (uid),
     KEY parent (pid),
