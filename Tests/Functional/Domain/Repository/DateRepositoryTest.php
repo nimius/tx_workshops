nimius/tx_workshops

commit 18883f6d9879f1bdc216bc2a6567b3043aff773a

TASK | Update constraints to make date fetching more foolproof
When e.g. a workshop is being deleted without its dates being deleted,
the upcomingDates view may yield unexpected rows.

Classes/Domain/Repository/DateRepository.php
@@ -76,7 +76,12 @@ class DateRepository extends Repository
             unset($categoriesConstraints);
         }
         if ($proxy->getHideChildDates()) {
+            // Child dates obviously have a parent set.
             $constraints[] = $query->equals('parent', 0);
+
+            // Additional failproofing if dates are available without a
+            // valid workshop, or a workshop had dates and switched type.
+            $constraints[] = $query->greaterThan('workshop.uid', 0);
         }
 
         if (!empty($constraints)) {

commit 13a2aa787c826a5060d4215ddd1589fce4df10f4

BUGFIX | Fix failproofing introduced in 18883f6 and fix functional tests

Classes/Domain/Repository/DateRepository.php
@@ -81,7 +81,7 @@ class DateRepository extends Repository
 
             // Additional failproofing if dates are available without a
             // valid workshop, or a workshop had dates and switched type.
-            $constraints[] = $query->greaterThan('workshop.uid', 0);
+            $constraints[] = $query->greaterThan('workshop', 0);
         }
 
         if (!empty($constraints)) {

Tests/Functional/Domain/Repository/DateRepositoryTest.php
@@ -82,12 +82,14 @@ class DateRepositoryTest extends \TYPO3\CMS\Core\Tests\FunctionalTestCase
     public function findByProxyRespectsStoragePid()
     {
         $date = $this->createDate();
+        $date->setWorkshop($this->createWorkshop());
         $date->setPid(8);
         $this->dateRepository->add($date);
         $this->persistenceManager->persistAll();
         
         $proxy = $this->createProxy();
         $proxy->setPid(8);
+        $proxy->setHidePastDates(false);
         
         $dates = $this->dateRepository->findByProxy($proxy);
         $this->assertTrue(count($dates) == 1);
@@ -101,6 +103,7 @@ class DateRepositoryTest extends \TYPO3\CMS\Core\Tests\FunctionalTestCase
     public function findByProxyRespectsHidePastDates()
     {
         $date = $this->createDate();
+        $date->setWorkshop($this->createWorkshop());
         $date->setEndAt(strtotime('-2 days'));
         $this->dateRepository->add($date);
         $this->persistenceManager->persistAll();
@@ -123,6 +126,7 @@ class DateRepositoryTest extends \TYPO3\CMS\Core\Tests\FunctionalTestCase
     public function findByProxyRespectsHideAlreadyStartedDates()
     {
         $date = $this->createDate();
+        $date->setWorkshop($this->createWorkshop());
         $date->setBeginAt(strtotime('-2 days'));
         $this->dateRepository->add($date);
         $this->persistenceManager->persistAll();
@@ -145,14 +149,17 @@ class DateRepositoryTest extends \TYPO3\CMS\Core\Tests\FunctionalTestCase
     public function findByProxyRespectsWithinDaysFromNow()
     {
         $date = $this->createDate();
+        $date->setWorkshop($this->createWorkshop());
         $date->setBeginAt(strtotime('+2 days'));
         $this->dateRepository->add($date);
         
         $date = $this->createDate();
+        $date->setWorkshop($this->createWorkshop());
         $date->setBeginAt(strtotime('+8 days'));
         $this->dateRepository->add($date);
 
         $date = $this->createDate();
+        $date->setWorkshop($this->createWorkshop());
         $date->setBeginAt(strtotime('+1 year'));
         $this->dateRepository->add($date);
 
@@ -173,7 +180,8 @@ class DateRepositoryTest extends \TYPO3\CMS\Core\Tests\FunctionalTestCase
      *
      * @return DateRepositoryProxy
      */
-    protected function createProxy() {
+    protected function createProxy()
+    {
         return $this->objectManager->get(DateRepositoryProxy::class);
     }
 
@@ -182,8 +190,13 @@ class DateRepositoryTest extends \TYPO3\CMS\Core\Tests\FunctionalTestCase
      *
      * @return Workshop
      */
-    protected function createWorkshop() {
-        return $this->objectManager->get(Workshop::class);
+    protected function createWorkshop()
+    {
+        $workshop = $this->objectManager->get(Workshop::class);
+        $workshop->setPid(2);
+        $this->workshopRepository->add($workshop);
+        $this->persistenceManager->persistAll();
+        return $workshop;
     }
 
     /**
@@ -191,7 +204,8 @@ class DateRepositoryTest extends \TYPO3\CMS\Core\Tests\FunctionalTestCase
      *
      * @return Category
      */
-    protected function createDate() {
+    protected function createDate()
+    {
         return $this->objectManager->get(Date::class);
     }
 

Tests/Functional/Domain/Repository/WorkshopRepositoryTest.php
@@ -230,7 +230,8 @@ class WorkshopRepositoryTest extends \TYPO3\CMS\Core\Tests\FunctionalTestCase
      *
      * @return WorkshopRepositoryProxy
      */
-    protected function createProxy() {
+    protected function createProxy()
+    {
         return $this->objectManager->get(WorkshopRepositoryProxy::class);
     }
 
@@ -239,7 +240,8 @@ class WorkshopRepositoryTest extends \TYPO3\CMS\Core\Tests\FunctionalTestCase
      *
      * @return Workshop
      */
-    protected function createWorkshop() {
+    protected function createWorkshop()
+    {
         return $this->objectManager->get(Workshop::class);
     }
 
@@ -248,7 +250,8 @@ class WorkshopRepositoryTest extends \TYPO3\CMS\Core\Tests\FunctionalTestCase
      *
      * @return Category
      */
-    protected function createCategory() {
+    protected function createCategory()
+    {
         return $this->objectManager->get(Category::class);
     }
 
@@ -257,7 +260,8 @@ class WorkshopRepositoryTest extends \TYPO3\CMS\Core\Tests\FunctionalTestCase
      *
      * @return Category
      */
-    protected function createDate() {
+    protected function createDate()
+    {
         return $this->objectManager->get(Date::class);
     }
 